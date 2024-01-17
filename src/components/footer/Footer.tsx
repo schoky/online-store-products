0cf0b463c882369894c213a5d98589f78262aedb
schoky <danya.safonov.07@mail.ru>
2024-01-17 20:47:24 +0300

    init

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,34 @@
+import style from './Footer.module.css';
+import githubLogo from '../../assets/images/github.png';
+
+export const Footer = () => {
+  return (
+    <footer className={style.footerWrapper}>
+      <div className={style.footer}>
+        <div className={style.githubWrapper}>
+          <a
+            className={style.githubLink}
+            href="https://github.com/schoky"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className={style.githubLogo} src={githubLogo} alt="GitHub" />
+            <div className={style.githubName}>schoky</div>
+          </a>
+          <a
+            className={style.githubLink}
+            href="https://github.com/debabin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className={style.githubLogo} src={githubLogo} alt="GitHub" />
+            <div className={style.githubName}>debabin</div>
+          </a>
+        </div>
+        <div className={style.footer}>
+          <p>Online store 2024</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
