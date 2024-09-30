76c37ffb1b39c2769601127214c759eaea891ac1
danya <danya.safonov.07@mail.ru>
2024-09-30 14:16:05 +0300

    init

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,15 +6,6 @@ export const Footer = () => {
     <footer className={style.footerWrapper}>
       <div className={style.footer}>
         <div className={style.githubWrapper}>
-          <a
-            className={style.githubLink}
-            href="https://github.com/schoky"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img className={style.githubLogo} src={githubLogo} alt="GitHub" />
-            <div className={style.githubName}>schoky</div>
-          </a>
         </div>
         <div className={style.footer}>
           <p>Online store 2024</p>
